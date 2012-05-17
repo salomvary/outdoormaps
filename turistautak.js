f4159dab2d906ed72d5d960395ec41e6fe5d6fc2
Márton Salomváry <salomvary@gmail.com>
2012-05-17 23:03:33 +0200

    Initial commit

---
+++ turistautak.js
@@ -0,0 +1,54 @@
+var turistautak = {};
+
+turistautak.LINES = new google.maps.ImageMapType({
+	getTileUrl: function(coord, zoom) {
+		return 'http://map.turistautak.hu/tiles/lines/' + zoom +
+			'/' + coord.x + '/' + coord.y + '.png';
+  },
+  tileSize: new google.maps.Size(256, 256),
+  maxZoom: 21,
+  minZoom: 0,
+	name: 'turistautak.hu vonalak és pontok'
+});
+
+turistautak.DEFAULT = new google.maps.ImageMapType({
+	getTileUrl: function(coord, zoom) {
+		// TODO rotate a/b/c/d
+		return 'http://a.map.turistautak.hu/tiles/turistautak/' + zoom +
+			'/' + coord.x + '/' + coord.y + '.png';
+  },
+  tileSize: new google.maps.Size(256, 256),
+  maxZoom: 22,
+  minZoom: 0,
+	name: 'turistautak.hu'
+});
+
+var locate = document.getElementById('locate');
+if(navigator.geolocation) {
+	locate.onclick = function() {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	};
+} else {
+	locate.style.display = 'none';
+}
+
+var mapOptions = {
+	center: new google.maps.LatLng(47.3, 19.5),
+	zoom: 8,
+	mapTypeId: 'turistautak',
+	mapTypeControlOptions: {
+		mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'turistautak']		
+	}	
+};
+
+function showPosition(position) {
+	if(position && ! ('code' in position)) {
+		map.setCenter(new google.maps.LatLng(position.coords.latitude, 
+			position.coords.longitude));
+		map.setZoom(18);
+	}
+}
+
+var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+map.mapTypes.set('turistautak', turistautak.DEFAULT);
+map.mapTypes.set('turistautak_lines', turistautak.LINES);
