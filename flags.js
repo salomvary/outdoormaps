b7e0beb331e099823a9b98925f5615a48112f24a
Márton Salomváry <salomvary@gmail.com>
2012-06-25 14:24:44 +0200

    developer flags

---
+++ flags.js
@@ -0,0 +1,16 @@
+var flags = ['offlineEnabled', 'doNotTrack'],
+	form = document.forms[0];
+
+flags.forEach(function(flag) {
+	form.innerHTML += '<p><label><input type="checkbox" name="'+flag+'"' + 
+		(localStorage[flag] ? ' checked="checked"' : '') + '>' +
+		flag + '</label></p>';
+});
+
+form.addEventListener('change', function(e) {
+	if(e.target.checked) {
+		localStorage[e.target.name] = true;
+	} else {
+		delete localStorage[e.target.name];
+	}
+}, false);
