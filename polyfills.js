b680f58ce21c0f3359159d335fd922dac5fc90ea
Márton Salomváry <salomvary@gmail.com>
2012-06-26 17:44:14 +0200

    support older browsers

--- polyfills.js
+++ polyfills.js
@@ -22,3 +22,15 @@ if (!Function.prototype.bind) {
     return fBound;
   };
 }
+
+// source: https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Object/create/#Polyfill
+if (!Object.create) {
+    Object.create = function (o) {
+        if (arguments.length > 1) {
+            throw new Error('Object.create implementation only accepts the first parameter.');
+        }
+        function F() {}
+        F.prototype = o;
+        return new F();
+    };
+}
