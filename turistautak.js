30d82b9cb96d9a737f1f339eb38d4de475b656bc
Márton Salomváry <salomvary@gmail.com>
2013-04-02 22:54:13 +0200

    fixed missing `geo` dependency

--- turistautak.js
+++ turistautak.js
@@ -3,7 +3,7 @@ window.addToHomeConfig = {
 };
 
 require.config({
-  deps: ['polyfills', 'vendor/add-to-homescreen/src/add2home'],
+  deps: ['polyfills', 'vendor/add-to-homescreen/src/add2home', 'vendor/geo'],
   paths: {
     'vendor/leaflet': 'vendor/leaflet.js/leaflet-src'
   },
