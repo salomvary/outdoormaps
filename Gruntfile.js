fb43294597db1ee40939edb8645bb5761ecb1ae6
Márton Salomváry <salomvary@gmail.com>
2013-04-13 18:01:03 +0200

    next level of gruntification

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,90 +1,255 @@
-var AssetGraph = require('assetgraph');
+'use strict';
+var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  var minifiedApp = 'turistautak.min.js';
+    // configurable paths
+    var yeomanConfig = {
+        app: 'app',
+        dist: 'dist'
+    };
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    requirejs: {
-      compile: {
-        options: {
-          baseUrl: '.',
-          mainConfigFile: 'turistautak.js',
-          name: 'vendor/almond',
-          include: 'turistautak',
-          insertRequire: ['turistautak'],
-          out: minifiedApp
+    grunt.initConfig({
+        yeoman: yeomanConfig,
+        watch: {
+            livereload: {
+                files: [
+                    '<%= yeoman.app %>/*.html',
+                    '{.tmp,<%= yeoman.app %>}{,*/}*.css',
+                    '{.tmp,<%= yeoman.app %>}{,*/}*.js',
+                    '<%= yeoman.app %>{,*/}*.{png,jpg,jpeg,webp}'
+                ],
+                tasks: ['livereload']
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            lrSnippet,
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, 'app')
+                        ];
+                    }
+                }
+            },
+            test: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, 'test')
+                        ];
+                    }
+                }
+            },
+            dist: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            mountFolder(connect, 'dist')
+                        ];
+                    }
+                }
+            }
+        },
+        open: {
+            server: {
+                path: 'http://localhost:<%= connect.options.port %>'
+            }
+        },
+        clean: {
+            dist: ['.tmp', '<%= yeoman.dist %>/*'],
+            server: '.tmp'
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                '<%= yeoman.app %>/{,*/}*.js',
+                '!<%= yeoman.app %>/vendor/*',
+                'test/{,*/}*.js'
+            ]
+        },
+        mocha: {
+            all: {
+                options: {
+                    run: true,
+                    urls: ['http://localhost:<%= connect.options.port %>/index.html']
+                }
+            }
+        },
+        // not used since Uglify task does concat,
+        // but still available if needed
+        /*concat: {
+            dist: {}
+        },*/
+        requirejs: {
+            dist: {
+                // Options: https://github.com/jrburke/r.js/blob/master/build/example.build.js
+                options: {
+                    // `name` and `out` is set by grunt-usemin
+                    baseUrl: 'app',
+                    optimize: 'none',
+                    // TODO: Figure out how to make sourcemaps work with grunt-usemin
+                    // https://github.com/yeoman/grunt-usemin/issues/30
+                    //generateSourceMaps: true,
+                    // required to support SourceMaps
+                    // http://requirejs.org/docs/errors.html#sourcemapcomments
+                    preserveLicenseComments: false,
+                    useStrict: true,
+                    wrap: true,
+                    insertRequire: ['turistautak']
+                    //uglify2: {} // https://github.com/mishoo/UglifyJS2
+                }
+            }
+        },
+        useminPrepare: {
+            html: '<%= yeoman.app %>/index.html',
+            options: {
+                dest: '<%= yeoman.dist %>'
+            }
+        },
+        cssjoin: {
+          dist: {
+            options: {
+              paths: ['app/']
+            },
+            files: {
+              'dist/app.css': ['app/app.css']
+            }
+          }
+        },
+        rev: {
+          options: {
+            algorithm: 'md5',
+            length: 8
+          },
+          assets: {
+            files: [{
+              src: [
+                'dist/*.{js,css,png,jpg}'
+              ]
+            }]
+          }
+        },
+        usemin: {
+            html: ['<%= yeoman.dist %>/*.html'],
+            css: ['<%= yeoman.dist %>/*.css'],
+            options: {
+                dirs: ['<%= yeoman.dist %>']
+            }
+        },
+        imagemin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= yeoman.app %>',
+                    src: '{,*/}*.{png,jpg,jpeg}',
+                    dest: '<%= yeoman.dist %>'
+                }]
+            }
+        },
+        cssmin: {
+            dist: {
+                files: {
+                    '<%= yeoman.dist %>/app.css': [
+                        '<%= yeoman.dist %>/{,*/}*.css'
+                    ]
+                }
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    /*removeCommentsFromCDATA: true,
+                    // https://github.com/yeoman/grunt-usemin/issues/44
+                    //collapseWhitespace: true,
+                    collapseBooleanAttributes: true,
+                    removeAttributeQuotes: true,
+                    removeRedundantAttributes: true,
+                    useShortDoctype: true,
+                    removeEmptyAttributes: true,
+                    removeOptionalTags: true*/
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= yeoman.app %>',
+                    src: '*.html',
+                    dest: '<%= yeoman.dist %>'
+                }]
+            }
+        },
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= yeoman.app %>',
+                    dest: '<%= yeoman.dist %>',
+                    src: [
+                        'vendor/leaflet.js/images/marker-*',
+                        '*.{ico,txt}',
+                        '.htaccess'
+                    ]
+                }]
+            }
         }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-volo');
+    grunt.renameTask('regarde', 'watch');
 
-  grunt.registerTask('assetgraph', function() {
-    var done = this.async();
-
-    new AssetGraph({root: './'})
-      .loadAssets('index.html')
-      .loadAssets(minifiedApp)
-      .loadAssets('vendor/leaflet.js/images/*')
-      .populate({
-        followRelations: { to: { url: /^file:/ } },
-        onError: function(err) {
-          grunt.log.error('error populating aseet graph ' + err);
-          done(false);
+    grunt.registerTask('server', function (target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
         }
-      })
-      .queue(function(graph) {
-        // replace requirejs script tag with
-        // the plain all-in-one compiled script
-        var requireMain = graph.findRelations({
-          type: 'HtmlRequireJsMain'
-        })[0];
-        var compiledScript = graph.findAssets({
-          fileName: minifiedApp
-        })[0];
-        var compiledHtmlScript = new graph.HtmlScript({
-          to: compiledScript
-        });
-        compiledHtmlScript.attach(requireMain.from, 'after', requireMain);
-        var requireJsScript = graph.findAssets({
-          fileName: 'require.js'
-        })[0];
-        graph.removeAsset(requireMain.to, true);
-        graph.removeAsset(requireJsScript, true);
-      })
-      .writeStatsToStderr()
-      .bundleRelations({ type: ['HtmlStyle'] })
-      .moveAssetsInOrder({ type: ['JavaScript', 'Css', 'Png'] }, function (asset) {
-        return asset.url + '?' + asset.md5Hex.substr(0, 10);
-      })
-      .addCacheManifest()
-      .queue(function(graph) {
-        // add marker images (referenced from js)
-        // to the manifest manually
-        var manifest = graph.findRelations({
-          type: 'HtmlCacheManifest'
-        })[0].to;
-        graph.findAssets({ url: /vendor\/leaflet\.js\/images\/marker-.*/ }).forEach(function(asset) {
-          new AssetGraph.CacheManifestEntry({
-            sectionName: 'CACHE',
-            to: asset
-          }).attach(manifest, 'last');
-        });
-      })
-      .writeAssetsToDisc({}, 'file://gh-pages/')
-      .run(function(err) {
-        if(err) {
-          grunt.log.error('error running asset graph ' + err);
-          done(false);
-        }
-      });
-  });
 
-  grunt.registerTask('compile', [ 'requirejs:compile', 'assetgraph' ]);
-  grunt.registerTask('build', [ 'volo:install', 'compile' ]);
+        grunt.task.run([
+            'clean:server',
+            'livereload-start',
+            'connect:livereload',
+            'open',
+            'watch'
+        ]);
+    });
 
+    grunt.registerTask('test', [
+        'clean:server',
+        'connect:test',
+        'mocha'
+    ]);
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'useminPrepare',
+        'requirejs',
+        'imagemin',
+        'htmlmin',
+        'concat',
+        'cssjoin',
+        'cssmin',
+        'uglify',
+        'copy',
+        'rev',
+        'usemin'
+    ]);
+
+    grunt.registerTask('default', [
+        'jshint',
+        'test',
+        'build'
+    ]);
 };
