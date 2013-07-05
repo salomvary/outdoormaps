acf874940e64a0d1a7e4e38c23ea96247c17c037
Márton Salomváry <salomvary@gmail.com>
2013-07-05 06:54:55 +0200

    Object.keys polyfill for Opera Mini

--- app/polyfills.js
+++ app/polyfills.js
@@ -24,6 +24,7 @@ if (!Function.prototype.bind) {
 }
 
 // source: https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Object/create/#Polyfill
+// for: Opera Mini
 if (!Object.create) {
     Object.create = function (o) {
         if (arguments.length > 1) {
@@ -33,4 +34,39 @@ if (!Object.create) {
         F.prototype = o;
         return new F();
     };
+}
+
+// source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+if (!Object.keys) {
+  Object.keys = (function () {
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
+        dontEnums = [
+          'toString',
+          'toLocaleString',
+          'valueOf',
+          'hasOwnProperty',
+          'isPrototypeOf',
+          'propertyIsEnumerable',
+          'constructor'
+        ],
+        dontEnumsLength = dontEnums.length;
+
+    return function (obj) {
+      if (typeof obj !== 'object' && typeof obj !== 'function' || obj === null) throw new TypeError('Object.keys called on non-object');
+
+      var result = [];
+
+      for (var prop in obj) {
+        if (hasOwnProperty.call(obj, prop)) result.push(prop);
+      }
+
+      if (hasDontEnumBug) {
+        for (var i=0; i < dontEnumsLength; i++) {
+          if (hasOwnProperty.call(obj, dontEnums[i])) result.push(dontEnums[i]);
+        }
+      }
+      return result;
+    };
+  })();
 }
