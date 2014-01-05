ad4c0c179c966befbb541c745d1f663367504119
Márton Salomváry <salomvary@gmail.com>
2014-01-05 23:38:13 +0100

    prompt for reload on appcache update

--- app/app.js
+++ app/app.js
@@ -21,3 +21,14 @@ require.config({
 require(['map'], function(Map) {
   new Map();
 });
+
+// check for application cache updates
+if (window.applicationCache) {
+  window.applicationCache.addEventListener('updateready', function() {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      if (window.confirm('A new version of Outdoor Maps is available. Load update?')) {
+        window.location.reload();
+      }
+    }
+  }, false);
+}
