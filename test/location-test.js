3b8bd2ca4608761144815839fd0ff328e1fe61ce
Márton Salomváry <salomvary@gmail.com>
2014-05-12 21:54:52 +0200

    fix location tests

--- test/location-test.js
+++ test/location-test.js
@@ -2,7 +2,7 @@ var Location = require('location');
 
 suite('Location', function() {
   test('parse', function() {
-    expect(Location.parse('osm,19.5,52.1,13'))
+    expect(Location.parse('osm/19.5/52.1/13'))
       .deep.equal({layers: ['osm'], center: {lat: 19.5, lng: 52.1}, zoom: 13});
     expect(Location.parse('crap'))
       .deep.equal({}, 'empty state');
@@ -10,6 +10,6 @@ suite('Location', function() {
 
   test('serialize', function() {
     expect(Location.serialize({layers: ['osm'], center: {lat: 19.5, lng: 52.1}, zoom: 13}))
-      .equal('osm,19.5,52.1,13');
+      .equal('osm/19.5/52.1/13');
   });
 });
