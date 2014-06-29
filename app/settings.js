91157fcd0a00d5f6f6a8d76272c848e9968194e3
Márton Salomváry <salomvary@gmail.com>
2014-06-30 01:22:01 +0200

    settings for overlays

--- app/settings.js
+++ app/settings.js
@@ -13,7 +13,9 @@ module.exports = klass({
     $.fastClick(closeButton);
 
     // initial state
-    this.mapLayer = (options.get('layers') || controller.defaults.layers)[0];
+    var layers = (options.get('layers') || controller.defaults.layers);
+    this.mapLayer = layers[0];
+    this.overlay = layers[1];
     this.mapType = Layers.get(this.mapLayer).mapType;
     this.defaultLayers = options.get('defaultLayers');
     if(! this.defaultLayers) {
@@ -43,18 +45,25 @@ module.exports = klass({
     var type = event.target.name;
     var layerId = this.defaultLayers[type] || Layers.keys(type)[0].id;
     this.mapType = type;
-    this.setLayers([layerId]);
+    this.setLayers([layerId, this.overlay]);
   },
 
   setMapLayer: function(event) {
     var id = event.target.name;
     this.defaultLayers[Layers.get(id).mapType] = id;
-    this.setLayers([id]);
+    this.setLayers([id, this.overlay]);
+  },
+
+  setOverlay: function(event) {
+    var id = event.target.name,
+        add = this.overlay != id;
+    this.setLayers([this.mapLayer, add && id]);
   },
 
   setLayers: function(layerIds) {
     this.mapLayer = layerIds[0];
-    this.controller.setLayers(layerIds);
+    this.overlay = layerIds[1];
+    this.controller.setLayers(layerIds.filter(Boolean));
     this.options.set({defaultLayers: this.defaultLayers});
     this.updateButtons();
   },
@@ -70,34 +79,16 @@ module.exports = klass({
       $.on(button, 'click', this.setMapType, this);
     }, this);
 
-
     // create layer type selection for map types with multiple
     // layers
     this.mapLayerGroups = ['hiking', 'satellite', 'map']
       .map(function(type) { return Layers.keys(type); })
       .filter(function(layers) { return layers.length > 1; })
-      .map(function(layers) {
-        var group = {mapType: layers[0].mapType};
+      .map(makeLayerGroups.bind(this, mapTypes, this.setMapLayer));
 
-        var g = document.createElement('fieldset');
-        g.className = 'map-layer control-group';
-        mapTypes.appendChild(g);
-        group.wrapper = g;
-
-        group.layers = layers.map(function(key) {
-          var button = document.createElement('button');
-          button.className = 'radio-btn';
-          button.name = key.id;
-          button.innerHTML = key.title;
-          //button.style.width = (100 / layers.length) + '%';
-          g.appendChild(button);
-
-          $.on(button, 'click', this.setMapLayer, this);
-          return button;
-        }, this);
-
-        return group;
-      }, this);
+    // overlay selection
+    this.overlayButtons =
+      makeLayerGroups.call(this, mapTypes, this.setOverlay, Layers.keys('overlay'));
   },
 
   updateButtons: function() {
@@ -111,17 +102,47 @@ module.exports = klass({
     this.mapLayerGroups.forEach(function(group) {
       if(group.mapType === this.mapType) {
         $.show(group.wrapper);
-        // toggle buttons state in active group
-        group.layers.forEach(function(button) {
-          $.toggleClass(button, 'active', button.name === this.mapLayer);
-        }, this);
+        toggleButtons(group.layers, this.mapLayer);
       } else {
         $.hide(group.wrapper);
       }
     }, this);
+
+    // toggle overlay
+    toggleButtons(this.overlayButtons.layers, this.overlay);
 
     // XXX fix a strange Chrome issue that settings doesn't repaint
     document.getElementById('settings')
       .style.opacity = 1;
   }
 });
+
+function makeLayerGroups(mapTypes, handler, layers) {
+  var group = {mapType: layers[0].mapType};
+
+  var g = document.createElement('fieldset');
+  g.className = 'map-layer control-group';
+  mapTypes.appendChild(g);
+  group.wrapper = g;
+
+  group.layers = layers.map(function(key) {
+    var button = document.createElement('button');
+    button.className = 'radio-btn';
+    button.name = key.id;
+    button.innerHTML = key.title;
+    //button.style.width = (100 / layers.length) + '%';
+    g.appendChild(button);
+
+    $.on(button, 'click', handler, this);
+    return button;
+  }, this);
+
+  return group;
+}
+
+function toggleButtons(buttons, activeButton) {
+  // toggle buttons state in active group
+  buttons.forEach(function(button) {
+    $.toggleClass(button, 'active', button.name === activeButton);
+  }, this);
+}
