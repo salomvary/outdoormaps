b80034971f770ed5d0a7fba0ea49f954805a37db
Márton Salomváry <salomvary@gmail.com>
2018-08-17 10:38:20 +0200

    Add webpack to the soup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  node: false,
+  entry: './app/app',
+  output: {
+    path: path.resolve(__dirname, 'app')
+  },
+  module: {
+    rules: [
+      {
+        test: require.resolve('./app/vendor/OS'),
+        use: 'imports-loader?proj4leaflet=vendor/proj4leaflet,MapMultiCRS=vendor/MapMultiCRS'
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'app')],
+    alias: {
+      'vendor/leaflet': 'vendor/leaflet/leaflet-src',
+      'proj4': 'vendor/proj4',
+      'polyline': 'vendor/polyline',
+      'corslite': 'vendor/corslite',
+      'leaflet': 'vendor/leaflet/leaflet-src'
+    }
+  }
+};
