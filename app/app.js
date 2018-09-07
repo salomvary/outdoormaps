145bf2ab36d27f92d0849c70b43cbf72e6b87062
Márton Salomváry <salomvary@gmail.com>
2018-09-07 22:33:32 +0200

    Add precache service worker

--- app/app.js
+++ app/app.js
@@ -8,12 +8,24 @@ window.addToHomeConfig = {
 new Map();
 
 // check for application cache updates
-if (window.applicationCache) {
-  window.applicationCache.addEventListener('updateready', function() {
-    if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
-      if (window.confirm('A new version of Outdoor Maps is available. Load update?')) {
-        window.location.reload();
+if ('serviceWorker' in navigator) {
+  var sw = navigator.serviceWorker.getRegistration();
+  if (sw) {
+    // See https://github.com/GoogleChromeLabs/sw-precache/blob/master/demo/app/js/service-worker-registration.js
+    sw.then(function(reg) {
+      if (reg) {
+        reg.onupdatefound = function() {
+          var installingWorker = reg.installing;
+
+          installingWorker.onstatechange = function() {
+            if (installingWorker.state === 'installed' && navigator.serviceWorker.controller) {
+              if (window.confirm('A new version of Outdoor Maps is available. Load update?')) {
+                window.location.reload();
+              }
+            }
+          };
+        };
       }
-    }
-  }, false);
+    });
+  }
 }
