c158facb54a2f0464e8cbb494990d682f2f66c8f
Márton Salomváry <salomvary@gmail.com>
2019-03-24 10:12:23 +0100

    Add source map

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
   node: false,
   context: path.resolve(__dirname, 'app'),
   entry: './app',
