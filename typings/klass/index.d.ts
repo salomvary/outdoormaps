7ef2b14d54e81febdb88fe4e3454f0edf4216b66
Márton Salomváry <salomvary@gmail.com>
2019-05-27 12:44:05 +0200

    Remove klass dependency

--- typings/klass/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'klass' {
-  interface ClassExtendOptions {
-    initialize?: Function;
-    [prop: string]: any;
-  }
-
-  export default function extend(options: ClassExtendOptions): any;
-}
