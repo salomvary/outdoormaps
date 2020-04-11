f9fc8d1fa3fd0a9d4b8f27df69a2521bb427dfcd
Márton Salomváry <salomvary@gmail.com>
2020-04-11 09:53:09 +0200

    Toggle fullscreen with keyboard

--- app/fullscreen.ts
+++ app/fullscreen.ts
@@ -7,13 +7,45 @@ import 'leaflet-fullscreen';
 export default class Fullscreen implements MapPlugin {
   private controller: Map
   private options: StateStore
+  private fullscreen: L.Control.Fullscreen;
 
   constructor(controller: Map, options: StateStore) {
     this.controller = controller;
     this.options = options;
+    this.fullscreen = new L.Control.Fullscreen({
+      title: {
+        'false': 'View Fullscreen (press f)',
+        'true': 'Exit Fullscreen (press f)'
+      }
+    });
   }
 
   setMap(map: L.Map) {
-    map.addControl(new L.Control.Fullscreen());
+    map.addControl(this.fullscreen);
+    // Prevent activating full screen when focus is somewhere else
+    // eg. typing into an input. Adopted from:
+    // https://github.com/Leaflet/Leaflet/blob/37d2fd15ad6518c254fae3e033177e96c48b5012/src/map/handler/Map.Keyboard.js
+    map.on('focus', this.addHook, this);
+    map.on('blur', this.removeHook, this);
+    L.DomEvent.on(map.getContainer(), 'focus', this.addHook, this);
+    L.DomEvent.on(map.getContainer(), 'blur', this.removeHook, this);
+    // Map is focused upon initialization
+    this.addHook();
+  }
+
+  private addHook() {
+    L.DomEvent.on(document.body, 'keydown', this.onKeyDown, this);
+  }
+
+  private removeHook() {
+    L.DomEvent.off(document.body, 'keydown', this.onKeyDown, this);
+  }
+
+  private onKeyDown(e: KeyboardEvent) {
+    if (e.altKey || e.ctrlKey || e.metaKey) { return; }
+
+    if (e.code === 'KeyF') {
+      this.controller.map.toggleFullscreen();
+    }
   }
 }
--- typings/leaflet-fullscreen/index.d.ts
+++ typings/leaflet-fullscreen/index.d.ts
@@ -4,7 +4,12 @@ import * as L from 'leaflet';
 declare module 'leaflet' {
   namespace Control {
     export class Fullscreen extends L.Control {
-      public constructor(options?: L.ControlOptions)
+      public constructor(options?: L.ControlOptions | {title?: {false?: string; true?: string}})
+      toggleFullscreen()
     }
+  }
+
+  interface Map {
+    toggleFullscreen();
   }
 }
